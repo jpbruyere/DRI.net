59eee9f612990e94fa2d15207bd6f43f39b47634
Jean-Philippe Bruyère <jp_bruyere@hotmail.com>
2017-05-23 14:23:35 +0200

    first commit

---
+++ DRI/Native.cs
@@ -0,0 +1,143 @@
+﻿//
+// DRM.cs
+//
+// Author:
+//       Jean-Philippe Bruyère <jp.bruyere@hotmail.com>
+//
+// Copyright (c) 2013-2017 Jean-Philippe Bruyère
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+using System;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+
+namespace DRI {
+	#region DRM callback signatures
+	[UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+	public delegate void VBlankCallback(int fd, int sequence, int tv_sec, int tv_usec, IntPtr user_data);
+	[UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+	public delegate void PageFlipCallback(int fd, int sequence, int tv_sec,	int tv_usec, ref int user_data);
+	#endregion
+
+	#region DRM enums
+	public enum EncoderType : uint
+	{
+		NONE=0,
+		DAC=1,
+		TMDS=2,
+		LVDS=3,
+		TVDAC=4,
+		VIRTUAL=5,
+		DSI=6,
+		DPMST=7,
+		DPI=8,
+	}
+	[Flags]public enum PageFlipFlags : uint
+	{
+		FlipEvent = 0x01,
+		FlipAsync = 0x02,
+		FlipFlags = FlipEvent | FlipAsync
+	}
+	/// <summary>Video mode flags, bit compatible with the xorg definitions. </summary>
+	[Flags]public enum VideoMode
+	{
+		PHSYNC = 0x01,
+		NHSYNC = 0x02,
+		PVSYNC = 0x04,
+		NVSYNC = 0x08,
+		INTERLACE = 0x10,
+		DBLSCAN = 0x20,
+		CSYNC = 0x40,
+		PCSYNC = 0x80,
+		NCSYNC = 0x10,
+		HSKEW = 0x0200,
+		BCAST = 0x0400,
+		PIXMUX = 0x0800,
+		DBLCLK = 0x1000,
+		CLKDIV2 = 0x2000,
+		//		FLAG_3D_MASK			(0x1f<<14)
+		//		FLAG_3D_NONE = 0x0;
+		//		FLAG_3D_FRAME_PACKING = 0x4000,
+		//		FLAG_3D_FIELD_ALTERNATIVE = 0x8000,
+		//		FLAG_3D_LINE_ALTERNATIVE	(3<<14)
+		//		FLAG_3D_SIDE_BY_SIDE_FULL	(4<<14)
+		//		FLAG_3D_L_DEPTH		(5<<14)
+		//		FLAG_3D_L_DEPTH_GFX_GFX_DEPTH	(6<<14)
+		//		FLAG_3D_TOP_AND_BOTTOM	(7<<14)
+		//		FLAG_3D_SIDE_BY_SIDE_HALF	(8<<14)
+	}
+	#endregion
+
+	#region DRM structs
+	[StructLayout(LayoutKind.Sequential)]
+	public struct EventContext
+	{
+		public int version;
+		public IntPtr vblank_handler;
+		public IntPtr page_flip_handler;
+		public static readonly int Version = 2;
+	}
+	[StructLayout(LayoutKind.Sequential)]
+	unsafe public struct drmFrameBuffer {
+		public uint fb_id;
+		public uint width, height;
+		public uint pitch;
+		public uint bpp;
+		public uint depth;
+		/* driver specific handle */
+		public uint handle;
+	}
+	[StructLayout(LayoutKind.Sequential)]
+	struct drmClip {
+		public ushort x1;
+		public ushort y1;
+		public ushort x2;
+		public ushort y2;
+	}
+	#endregion
+
+	public class Native {
+		#region PINVOKE
+		const string lib = "libdrm";
+		[DllImport(lib, CallingConvention = CallingConvention.Cdecl)]
+		internal static extern int drmHandleEvent(int fd, ref EventContext evctx);
+		[DllImport(lib, CallingConvention = CallingConvention.Cdecl)]
+		internal static extern int drmModeAddFB(int fd, uint width, uint height, byte depth, byte bpp,
+			uint stride, uint bo_handle, out uint buf_id);
+		[DllImport(lib, CallingConvention = CallingConvention.Cdecl)]
+		internal static extern int drmModeRmFB(int fd, uint bufferId);
+		[DllImport(lib, CallingConvention = CallingConvention.Cdecl)]
+		internal static extern int drmModeDirtyFB(int fd, uint bufferId, IntPtr clips, uint num_clips);
+		[DllImport(lib,CallingConvention = CallingConvention.Cdecl)]
+		unsafe internal static extern drmFrameBuffer* drmModeGetFB(int fd, uint fb_id);
+		[DllImport(lib, CallingConvention = CallingConvention.Cdecl)]
+		unsafe internal static extern void drmModeFreeFB(drmFrameBuffer* ptr);
+		[DllImport(lib, CallingConvention = CallingConvention.Cdecl)]
+		static extern int drmModePageFlip(int fd, uint crtc_id, uint fb_id, PageFlipFlags flags, ref int user_data);
+		[DllImport(lib, CallingConvention = CallingConvention.Cdecl)]
+		unsafe static extern int drmModeSetCrtc(int fd, uint crtcId, uint bufferId,	uint x, uint y, uint* connectors, int count, ref ModeInfo mode);
+		[DllImport(lib, CallingConvention = CallingConvention.Cdecl)]
+		internal static extern int drmModeSetCursor2(int fd, uint crtcId, uint bo_handle, uint width, uint height, int hot_x, int hot_y);
+		[DllImport(lib, CallingConvention = CallingConvention.Cdecl)]
+		internal static extern int drmModeMoveCursor(int fd, uint crtcId, uint x, uint y);
+		#endregion
+	}
+}
---
+++ Libc.cs
@@ -0,0 +1,166 @@
+﻿#region License
+//
+// Linux.cs
+//
+// Author:
+//       Stefanos A. <stapostol@gmail.com>
+//
+// Copyright (c) 2006-2014 Stefanos Apostolopoulos
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+#endregion
+
+using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+#pragma warning disable 0649 // field is never assigned
+
+namespace Linux
+{
+    partial class Libc
+    {
+        const string lib = "libc";
+
+        [DllImport(lib)]
+        public static extern int dup(int file);
+
+        [DllImport(lib)]
+        public static extern int dup2(int file1, int file2);
+
+        [DllImport(lib)]
+        public static extern int ioctl(int d, uint request, [Out] IntPtr data);
+
+        [DllImport(lib)]
+        public static extern int open([MarshalAs(UnmanagedType.LPStr)]string pathname, OpenFlags flags);
+
+        [DllImport(lib)]
+        public static extern int open(IntPtr pathname, OpenFlags flags);
+
+		[DllImport(lib)]
+		public static extern int posix_openpt (OpenFlags flags);
+
+        [DllImport(lib)]
+        public static extern int close(int fd);
+
+        [DllImport(lib)]
+        unsafe public static extern IntPtr read(int fd, void* buffer, UIntPtr count);
+		[DllImport(lib)]
+		unsafe public static extern uint write(int fd, void *buffer, int count); 
+
+		public static void write (int fd, byte[] b){
+			unsafe {
+				fixed (byte* pb = b) {
+					write (fd, pb, b.Length); 
+				}
+			}
+		}
+
+        public static int read(int fd, out byte b)
+        {
+            unsafe
+            {
+                fixed (byte* pb = &b)
+                {
+                    return read(fd, pb, (UIntPtr)1).ToInt32();
+                }
+            }
+        }
+
+        public static int read(int fd, out short s)
+        {
+            unsafe
+            {
+                fixed (short* ps = &s)
+                {
+                    return read(fd, ps, (UIntPtr)2).ToInt32();
+                }
+            }
+        }
+
+        [DllImport(lib)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool isatty(int fd);
+
+		[DllImport(lib)]
+		public static extern int getgid ();
+		[DllImport(lib)]
+		public static extern int setpgid (int pid, int pgid);
+		[DllImport(lib)]
+		public static extern int getpgid (int pid);
+
+		//public static extern int setpgrp (int pgid);
+		[DllImport(lib)]
+		public static extern int getpgrp ();
+    }
+
+    enum ErrorNumber
+    {
+        Interrupted = 4,
+        Again = 11,
+        InvalidValue = 22,
+    }
+
+    [Flags]
+    enum DirectionFlags
+    {
+        None = 0,
+        Write = 1,
+        Read = 2
+    }
+
+    [Flags]
+    enum OpenFlags
+    {
+        ReadOnly = 0x0000,
+        WriteOnly = 0x0001,
+        ReadWrite = 0x0002,
+        NonBlock = 0x0800,
+        CloseOnExec = 0x0080000
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    struct Stat
+    {
+        public IntPtr dev;     /* ID of device containing file */
+        public IntPtr ino;     /* inode number */
+        public IntPtr mode;    /* protection */
+        public IntPtr nlink;   /* number of hard links */
+        public IntPtr uid;     /* user ID of owner */
+        public IntPtr gid;     /* group ID of owner */
+        public IntPtr rdev;    /* device ID (if special file) */
+        public IntPtr size;    /* total size, in bytes */
+        public IntPtr blksize; /* blocksize for file system I/O */
+        public IntPtr blocks;  /* number of 512B blocks allocated */
+        public IntPtr atime;   /* time of last access */
+        public IntPtr mtime;   /* time of last modification */
+        public IntPtr ctime;   /* time of last status change */
+    }
+
+    struct EvdevInputId
+    {
+        public ushort BusType;
+        public ushort Vendor;
+        public ushort Product;
+        public ushort Version;
+    }
+
+}
+
